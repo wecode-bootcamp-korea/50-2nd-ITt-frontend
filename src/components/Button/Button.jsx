159943a2fd31b2fc8394b470b750d6c657fec9c2
홍지영 <hjy961021@gmail.com>
2023-11-08 16:19:33 +0900

    feat : 공통 버튼 추가

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import './Button.scss';
 
-const Button = () => {
-  return <div>button</div>;
+const Button = props => {
+  const { btnLine, btnText, disabled, onClick } = props;
+  const _btnLine = btnLine ? 'btnLine' : '';
+  return (
+    <button
+      type="button"
+      className={`btn ${_btnLine}`}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {btnText}
+    </button>
+  );
 };
 
 export default Button;
