8890d6bdaad2046b6e0a1f2cb2b0fe43f00bd0a5
Haze10425 <jheee042500@gmail.com>
2023-11-09 03:38:24 +0900

    feat: 결제 페이지 UI 구현

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import Button from '../../components/Button/Button';
+import Ordersinfo from './components/Ordersinfo';
 import './Payment.scss';
 
 const Payment = () => {
-  return <div>payment</div>;
+  return (
+    <div className="payment">
+      <div className="paymentArea">
+        <h1 className="paymentTitle">예매정보</h1>
+        <Ordersinfo
+          title="뮤지컬 고양이"
+          imgUrl="/images/visual_01.png"
+          time="150분"
+          date="2023년 5월 5일 오후 2시"
+          location="수원아트센터"
+          seatNumber="R열 35번"
+          totalPrice="70,000"
+        />
+        <div className="tossArea">toss API</div>
+        <Button btnText="결제완료" />
+      </div>
+    </div>
+  );
 };
 
 export default Payment;
---
+++ src/pages/Payment/components/Ordersinfo.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import './Ordersinfo.scss';
+
+const Ordersinfo = ({
+  title,
+  imgUrl,
+  time,
+  date,
+  location,
+  seatNumber,
+  totalPrice,
+}) => {
+  const ORDERLABEL = [
+    { id: 1, type: '관람일자', content: date },
+    { id: 2, type: '관람시간', content: time },
+    { id: 3, type: '위치', content: location },
+    { id: 4, type: '좌석', content: seatNumber },
+  ];
+  return (
+    <div className="ordersInfo">
+      <div className="ordersInfoArea">
+        <div className="orderImgArea">
+          <img src={imgUrl} alt="orderImage" className="orderImage" />
+        </div>
+        <div className="orderInfo">
+          <div className="orderList">
+            <h1 className="title">{title}</h1>
+            <ul>
+              {ORDERLABEL.map(order => (
+                <li className="ticketInfo" key={order.type}>
+                  <span className="label">{order.type}</span>
+                  <span className="content">{order.content}</span>
+                </li>
+              ))}
+            </ul>
+            <div className="totalPriceArea">
+              <p className="label">총 결제금액</p>
+              <span className="content">
+                <span className="totalPrice">{totalPrice}</span>원
+              </span>
+            </div>
+          </div>
+          <div className="dashLine" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Ordersinfo;
