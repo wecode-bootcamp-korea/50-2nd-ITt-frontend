a5e50985a0f0a67d2e3ba38923afd40855349a70
Hyun Yong Jeong <jhy3579012@gmail.com>
2023-11-09 17:04:24 +0900

    Add: Main 페이지 UI 초안 작성

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,8 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { Main_Slide } from './MainData/data.js';
 import './Main.scss';
 
 const Main = () => {
-  return <div className="main">main</div>;
+  //   const [carouselIdx, setCarouselIdx] = useState(0);
+  //   const [bannerSlideIdx, setBannerSlideIdx] = useState(0);
+  //   const slideToLeft = () => {
+  //     if (bannerSlideIdx === 0) {
+  //       setBannerSlideIdx(Main_Slide.length - 1);
+  //     } else {
+  //       setBannerSlideIdx(prev => prev - 1);
+  //     }
+  //   };
+  //   const slideToRight = () => {
+  //     if (bannerSlideIdx === Main_Slide.length - 1) {
+  //       setBannerSlideIdx(0);
+  //     } else {
+  //       setBannerSlideIdx(prev => prev + 1);
+  //     }
+  //   };
+  //   return (
+  //     <div className="main">
+  //       {Main_Slide.length !== 0 && (
+  //         <div className="slidebox">
+  //           <ul
+  //             className="slidelist"
+  //             style={{
+  //               transform: `translateX(calc(-100% * ${bannerSlideIdx}))`,
+  //             }}
+  //           >
+  //             {Main_Slide.map((slide, id) => (
+  //               <li className="slideitem" key={id}>
+  //                 <img src={slide.image_source} />
+  //               </li>
+  //             ))}
+  //           </ul>
+  //           <div className="arrowContainer">
+  //             <img
+  //               className="arrow"
+  //               src="/images/left_arrow.png"
+  //               alt="left arrow"
+  //               onClick={slideToLeft}
+  //             />
+  //             <img
+  //               className="arrow"
+  //               src="/images/right_arrow.png"
+  //               alt="right arrow"
+  //               onClick={slideToRight}
+  //             />
+  //           </div>
+  //         </div>
+  //       )}
+  //     </div>
+  //   );
 };
 
 export default Main;
---
+++ src/pages/Main/MainData/data.js
@@ -0,0 +1,9 @@
+export const Main_Slide = [
+  { id: 1, image_source: 'public/images/slideimg1.png ' },
+
+  { id: 1, image_source: ' public/images/slideimg2.png' },
+
+  { id: 1, image_source: ' public/images/slideimg3.png' },
+
+  { id: 1, image_source: 'public/images/slideimg4.png ' },
+];
