d93e075abc2babb5d5ecdda8c90bc5238e010c40
Hyun Yong Jeong <jhy3579012@gmail.com>
2023-11-09 17:40:02 +0900

    추가 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,7 +5,7 @@ const Button = props => {
 <<<<<<< HEAD
   const { width, height, outline, btnText, disabled, onClick } = props;
 =======
-  const { width, height, outline, btnText, disabled, onClick, children } = props;
+  const { width, height, outline, btnText, disabled, onClick } = props;
 >>>>>>> main
   const _outline = outline ? 'btnLine' : '';
 
