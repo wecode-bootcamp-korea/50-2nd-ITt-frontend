973575846ef1897e2cd15af209a20f5636ca0884
홍지영 <hjy961021@gmail.com>
2023-11-09 14:46:52 +0900

    fix : 버튼 text 수정

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.scss';
 
 const Button = props => {
-  const { outline, btnText, disabled, onClick } = props;
+  const { outline, disabled, onClick, children } = props;
   const _outline = outline ? 'btnLine' : '';
 
   return (
@@ -12,7 +12,7 @@ const Button = props => {
       disabled={disabled}
       onClick={onClick}
     >
-      {btnText}
+      {children}
     </button>
   );
 };
