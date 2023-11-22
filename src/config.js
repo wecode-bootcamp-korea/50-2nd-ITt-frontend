d91a00a00bf4beb6ed30d783b9efc85b024d0513
Haze10425 <jheee042500@gmail.com>
2023-11-22 19:33:17 +0900

    fix: 라우터 수정

--- src/config.js
+++ src/config.js
@@ -2,42 +2,44 @@
 
 import axios from 'axios';
 
+const BASE_URL = 'http://10.58.52.201:8000';
+const loginToken = localStorage.getItem('token');
+
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
-const BASE_URL_ITEM = 'http://10.58.52.162:8000';
-// const BASE_URL = 'http://10.58.52.121:8000';
-const BASE_URL_ADMIN = 'http://10.58.52.115:8000';
-const BASE_URL_DETAIL = 'http://10.58.52.119:8000';
-const BASE_URL_ADVANCE = 'http://10.58.52.176:8000';
-const BASE_URL_ORDER = 'http://10.58.52.176:8000';
-const BASE_URL_MYPAGE = 'http://10.58.52.115:8000';
+axios.defaults.headers.common.Authorization = loginToken;
+axios.defaults.timeout = 2500;
+
+//로그인
+export const GET_LOGIN_API = `${BASE_URL}/users/adminlogin`;
+export const GET_KAKAO_LOGIN_API = `${BASE_URL}/users/kakaologin`;
 
 //관리자
-export const GET_ADMIN_SELECTLIST_API = `${BASE_URL_ADMIN}/admin/selectList`;
-export const GET_ADMIN_INSERTITENLIST_API = `${BASE_URL_ADMIN}/admin/insertItemList`;
-export const GET_ADMIN_SELECTITEMLIST_API = `${BASE_URL_ADMIN}/admin/selectItemList`;
-export const GET_ADMIN_UPDATITEMLIST_API = `${BASE_URL_ADMIN}/admin/
+export const GET_ADMIN_SELECTLIST_API = `${BASE_URL}/admin/selectList`;
+export const GET_ADMIN_INSERTITENLIST_API = `${BASE_URL}/admin/insertItemList`;
+export const GET_ADMIN_SELECTITEMLIST_API = `${BASE_URL}/admin/selectItemList`;
+export const GET_ADMIN_UPDATITEMLIST_API = `${BASE_URL}/admin/
 updateItemList`;
-export const GET_ADMIN_DELECTITEMLIST_API = `${BASE_URL_ADMIN}/admin/deleteItemList`;
-export const GET_ADMIN_SELECTORDERLIST_API = `${BASE_URL_ADMIN}/admin/selectOrderList`;
-export const GET_ADMIN_DELETEORDERLIST_API = `${BASE_URL_ADMIN}/admin/deleteOrderList`;
-export const GET_ADMIN_SELECTCATEGORYLIST_API = `${BASE_URL_ADMIN}/admin/selectCategoryList`;
+export const GET_ADMIN_DELECTITEMLIST_API = `${BASE_URL}/admin/deleteItemList`;
+export const GET_ADMIN_SELECTORDERLIST_API = `${BASE_URL}/admin/selectOrderList`;
+export const GET_ADMIN_DELETEORDERLIST_API = `${BASE_URL}/admin/deleteOrderList`;
+export const GET_ADMIN_SELECTCATEGORYLIST_API = `${BASE_URL}/admin/selectCategoryList`;
 
 //상세페이지
-export const GET_DETAIL_API = `${BASE_URL_DETAIL}/detail`;
-export const GET_SEAT_API = `${BASE_URL_DETAIL}/detail`;
-export const GET_ADVANCE_API = `${BASE_URL_ADVANCE}/order`;
+export const GET_DETAIL_API = `${BASE_URL}/detail`;
+export const GET_SEAT_API = `${BASE_URL}/detail`;
+export const GET_ADVANCE_API = `${BASE_URL}/order`;
 
 //결제페이지 유저정보
-export const GET_ORDER_API = `${BASE_URL_ORDER}/order`;
-export const GET_ITEM_API = `${BASE_URL_ITEM}/itemList`;
+export const GET_ORDER_API = `${BASE_URL}/order`;
+export const GET_ITEM_API = `${BASE_URL}/itemList`;
 // 결제페이지 결제 요청
-export const PUT_PAYMENT_API = `${BASE_URL_ORDER}/order/pointDeduction`;
+export const PUT_PAYMENT_API = `${BASE_URL}/order/pointDeduction`;
 
 //마이페이지 프로필 업로드
-export const PUT_PROFILE_API = `${BASE_URL_MYPAGE}/users/mypage/update`;
+export const PUT_PROFILE_API = `${BASE_URL}/users/mypage/update`;
 //마이페이지 결제취소
-export const POST_CANCEL_API = `${BASE_URL_MYPAGE}/users/mypage/cancel`;
+export const POST_CANCEL_API = `${BASE_URL}/users/mypage/cancel`;
 //마이페이지 유저정보
-export const GET_USER_API = `${BASE_URL_MYPAGE}/users/mypage`;
+export const GET_USER_API = `${BASE_URL}/users/mypage`;
 //마이페이지 유저 결제내역 정보
-export const GET_USER_ORDER_API = `${BASE_URL_ORDER}/order`;
+export const GET_USER_ORDER_API = `${BASE_URL}/order`;
